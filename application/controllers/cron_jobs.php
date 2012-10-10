386644d986ec8bf7aa7e5eea538708f4edde1030
Gabriel Marques <gabriel@gabrielmarques.com.br>
2012-10-10 19:50:00 -0300

    ok

--- application/controllers/cron_jobs.php
+++ application/controllers/cron_jobs.php
@@ -18,11 +18,25 @@ class Cron_jobs extends CI_Controller{
 		$this->load->model('apartments_model');
 	}
 
-	public function get_apartments($page = 1, $last_page = false, $debug = false){
+	//public function get_apartments($page = 1, $last_page = false, $debug = false){
+	public function get_apartments(){
 		if($this->input->is_cli_request()){
+			$hour = date('H');
+			$min = date('i');
+			//$hour = 0;
+			//$min = 0;
+
+			$hour_cnt = $hour * 10;
+			$min_cnt = floor($min / 5) * 2;
+			$page = $hour_cnt + $min_cnt + 1;
+			$last_page = $page + 2;
+			$debug = false;
+
+			/*
 			$page = is_numeric($page) ? $page : 1;
 			$last_page = empty($last_page) ? false : $last_page;
 			$debug = $debug == 'true' ? true : false;
+			*/
 
 			$success = $this->apartments_model->get_zap_apartments($page, $last_page, $debug);
 
